a0f214569e449cfca3a5d95f2002dd465500b982
hedhyw <hedhyw@yahoo.com>
2019-02-02 12:22:40 +0300

    Make private config

--- env/debug.sh
+++ env/debug.sh
@@ -2,15 +2,22 @@
 export IPBOT_LOGLEVEL="debug"
 
 # HTTP Proxy for connections
-export IPBOT_HTTP_PROXY="socks5://localhost:9050"
+export IPBOT_HTTP_PROXY="socks5://HOST:PORT"
 
 # Format is user1:user2:user3
-export IPBOT_USERS="hedhyw"
+export IPBOT_USERS="YOUR_USERNAME"
 
 # Contact @botfather
-export IPBOT_TG_TOKEN=""
+export IPBOT_TG_TOKEN="SECRET"
 export IPBOT_TG_DEBUG="true"
 export IPBOT_TG_TIMEOUT="256"
 
 # IFConfig settings
 export IPBOT_IFCONFIG_URL="https://ifconfig.me"
+
+if [ -f ./secret.sh ]; then
+  source ./secret.sh
+fi
+if [ -f ./env/secret.sh ]; then
+  source ./env/secret.sh
+fi
